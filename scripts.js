5bf92a1717ab3a07b4e4ca379f264dcd986e51e0
Kob <57989060+the-Kob@users.noreply.github.com>
2023-11-10 17:57:59 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -20,4 +20,15 @@ document.addEventListener('DOMContentLoaded', function () {
         listItem.appendChild(link);
         documentList.appendChild(listItem);
     });
+
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 });
