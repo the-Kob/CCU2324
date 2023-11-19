89ea7086b00054d8216565f390886cdc7d40051c
Guilherme Pereira <guilhermecnpereira@tecnico.ulisboa.pt>
2023-11-19 13:58:48 +0000

    error fixed

--- ccu/vue.config.js
+++ ccu/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "7CCU2324/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/CCU2324/" : "/",
 })
